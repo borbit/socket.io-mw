51d723419e1719bd36ec262d63a32b0853b18047
Serge Borbit <serge.borbit@gmail.com>
2013-03-24 17:55:46 +0200

    0.0.3 passing event name through the payload object

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ var _ = require('underscore')
   , socketio = require('socket.io')
   , async = require('async');
 
-function middleware(socket, stack) {
+function middleware(event, socket, stack) {
   return function(req, callback) {
     // get the client ip address
     var ip = socket.handshake.headers['x-forwarded-for'] ||
              socket.handshake.address.address;
-             
-    var payload = {req: req || {}, res: {}, ip: ip};
-    
+
+    var payload = {
+      event: event
+    , req: req || {}
+    , res: {}
+    , ip: ip
+    };
+
     async.forEachSeries(stack, function(fn, next) {
       fn(socket, payload, next);
     }, function(err) {
       if (!callback) {
         return;
       }
-      
+
       if (err) {
         callback({error: parseError(err)});
       } else {
@@ -30,45 +35,45 @@ function parseError(err) {
   if (_.isFunction(err.toJSON)) {
     return err.toJSON();
   }
-  
+
   var result = {
     name: err.name
   , message: err.message
   };
-  
+
   if (err.data) {
     result.data = err.data;
   }
-  
+
   return result;
 }
 
 function mw(http) {
   var io = socketio.listen(http);
   var stacks = {};
-  
+
   io.sockets.on('connection', function(socket) {
     _.each(stacks, function(stack, event) {
-      socket.on(event, middleware(socket, stack));
+      socket.on(event, middleware(event, socket, stack));
     });
   });
-  
+
   return {
     on: function on(events) {
       var fn = _.rest(arguments);
-      
-      _.isArray(events) || (events = [events]);      
-      
+
+      _.isArray(events) || (events = [events]);
+
       events.forEach(function(event) {
         stacks[event] || (stacks[event] = []);
         stacks[event] = stacks[event].concat(fn);
       });
     },
-    
+
     configure: function() {
       return io.configure.apply(io, arguments);
     },
-  
+
     set: function() {
       return io.set.apply(io, arguments);
     }
